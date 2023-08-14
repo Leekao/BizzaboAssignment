e0036251217d24c5b6a568aee02f9b23a430cd8d
Erez Hochman <erez.hochman@gmail.com>
2023-08-14 22:00:27 +0300

    added prom-client, added metrics endpoint

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
 const express = require('express')
+const promClient = require('prom-client')
 const app = express()
 const PORT = 9999
 
+const defaultMetrics = promClient.collectDefaultMetrics
+defaultMetrics({timeout: 5000})
+
+const httpResponsesTotal = new promClient.Counter({
+  name: 'responses_total',
+  help: 'HTTP responses (Total)',
+  labelNames: ['status'],
+})
+
+app.use((req, res, next) => {
+  res.on('finish', () => {
+    httpResponsesTotal.inc({ status: res.statusCode })
+  })
+  next()
+})
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.get('/metrics', async (req, res) => {
+  res.set('Content-Type', promClient.register.contentType)
+  const metrics = await promClient.register.metrics()
+  res.end(metrics)
 })
 
 app.listen(PORT, () => {
