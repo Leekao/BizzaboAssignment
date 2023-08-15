172236d6dd0471f849da4722c58ec59b58160e18
Erez Hochman <erez.hochman@gmail.com>
2023-08-16 02:38:41 +0300

    changed errors to 1 minute window

--- webApp/index.js
+++ webApp/index.js
@@ -4,29 +4,40 @@ const app = express()
 const PORT = 9999
 
 const defaultMetrics = promClient.collectDefaultMetrics
-defaultMetrics({timeout: 5000})
+defaultMetrics({ timeout: 5000 })
 
-const httpResponsesTotal = new promClient.Counter({
-  name: 'http_responses_total',
-  help: 'HTTP responses (Total)',
-  labelNames: ['status'],
-})
+let responseTimestamps = []  // Queue to store response timestamps
 
 app.use((req, res, next) => {
   res.on('finish', () => {
-    httpResponsesTotal.inc({ status: res.statusCode })
+    const now = Date.now()
+
+    if (res.statusCode !== 200) {
+      responseTimestamps.push(now)
+    }
+    while (responseTimestamps.length && (now - responseTimestamps[0] > 60000)) {
+      responseTimestamps.shift()
+    }
   })
+
   next()
 })
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+const httpNon200ResponsesLastMinute = new promClient.Gauge({
+  name: 'http_non_200_responses_last_minute',
+  help: 'HTTP non-200 responses in the last minute',
 })
 
 app.get('/metrics', async (req, res) => {
+  httpNon200ResponsesLastMinute.set(responseTimestamps.length)
+
   res.set('Content-Type', promClient.register.contentType)
   const metrics = await promClient.register.metrics()
   res.end(metrics)
+})
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
 })
 
 app.listen(PORT, () => {
